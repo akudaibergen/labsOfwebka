4db426860af3de8c1594f85d34be175d8a22fb14
akudaibergen <kudaibergen.adil@gmail.com>
2020-02-29 08:59:23 +0600

    2

---
+++ week4/src/app/products.ts
@@ -0,0 +1,100 @@
+export interface Products{
+  name: string,
+  price: number,
+  description: string,
+  rating: number,
+  image: string,
+  url: string
+}
+
+export const products = [
+    {
+      image:"https://ae01.alicdn.com/kf/H0d1d3a5d66e54773a0943ad966147da6B/4Pcs-lot-Replacement-Electric-Toothbrush-Heads-For-EB-17P18202550-Hygiene-Care-Clean-Electric-Tooth-Brush-for.png_50x50.png",
+      name: 'Clean Electric Tooth Brush',
+      price: 799,
+      description: 'Soft brusing to take care of your teeth with a different color ring on each brush head, easy to distinguish',
+      rating:5.5,
+      url:"https://www.aliexpress.com/item/4000003734773.html?dp=52884308720285c0f8663b62e515588f&af=886947&cv=47843&afref=https%253A%252F%252Fcampaign.aliexpress.com%252Fwow%252Fgf%252Fupr-bypass-s%253Fspm%253Da2g0o.home.15027.8.340f2c25wVdGOc%2526wh_pid%253DValentines_Day_Computer_Home_Appliance%2526wh_weex%253Dtrue%2526preDownLoad%253Dtrue%2526preI&mall_affr=pr3&utm_source=admitad&utm_medium=cpa&utm_campaign=886947&utm_content=47843&dp=52884308720285c0f8663b62e515588f&af=886947&cv=47843&afref=https%253A%252F%252Fcampaign.aliexpress.com%252Fwow%252Fgf%252Fupr-bypass-s%253Fspm%253Da2g0o.home.15027.8.340f2c25wVdGOc%2526wh_pid%253DValentines_Day_Computer_Home_Appliance%2526wh_weex%253Dtrue%2526preDownLoad%253Dtrue%2526preI&mall_affr=pr3&utm_source=admitad&utm_medium=cpa&utm_campaign=886947&utm_content=47843&aff_request_id=d478da32326a495d9193d2fc650aa227-1581571389190-06064-VnYZvQVf&aff_platform=aaf&cpt=1581571389190&sk=VnYZvQVf&aff_trace_key=d478da32326a495d9193d2fc650aa227-1581571389190-06064-VnYZvQVf&terminal_id=4f7d8fd932bb404fa044fd99c5b1f517"
+    },
+    {
+      image:"https://ae01.alicdn.com/kf/HTB1ADThXBGE3KVjSZFhq6AkaFXa3/KERUI-M525-Home-Security-Welcome-Wireless-Doorbell-Smart-Chimes-Doorbell-Alarm-LED-light-32-Songs-with.jpg_50x50.jpg",
+      name: ' Wireless Doorbell Smart ChimesPhone Mini',
+      price: 699,
+      description: 'The wireless doorbell adopt advance wireless receiving and dispatching technology, powerful microcomputer control system, high capacity and superior voice processing technology, up to 32 songs with reliable and stable performance.',
+      rating:6.7,
+      url:"https://www.aliexpress.com/item/32818601228.html?dp=f2d0e01eb4b05b71ad949463ed646dac&af=886947&cv=47843&afref=https%253A%252F%252Fcampaign.aliexpress.com%252Fwow%252Fgf%252Fupr-bypass-s%253Fspm%253Da2g0o.home.15027.8.340f2c25wVdGOc%2526wh_pid%253DValentines_Day_Computer_Home_Appliance%2526wh_weex%253Dtrue%2526preDownLoad%253Dtrue%2526preI&mall_affr=pr3&utm_source=admitad&utm_medium=cpa&utm_campaign=886947&utm_content=47843&dp=f2d0e01eb4b05b71ad949463ed646dac&af=886947&cv=47843&afref=https%253A%252F%252Fcampaign.aliexpress.com%252Fwow%252Fgf%252Fupr-bypass-s%253Fspm%253Da2g0o.home.15027.8.340f2c25wVdGOc%2526wh_pid%253DValentines_Day_Computer_Home_Appliance%2526wh_weex%253Dtrue%2526preDownLoad%253Dtrue%2526preI&mall_affr=pr3&utm_source=admitad&utm_medium=cpa&utm_campaign=886947&utm_content=47843&aff_request_id=9cf8584688e04fb0a103acb60a8709b1-1581572559999-02246-VnYZvQVf&aff_platform=aaf&cpt=1581572559999&sk=VnYZvQVf&aff_trace_key=9cf8584688e04fb0a103acb60a8709b1-1581572559999-02246-VnYZvQVf&terminal_id=4f7d8fd932bb404fa044fd99c5b1f517"
+    },
+    {
+      image:"https://ae01.alicdn.com/kf/H411f5b418bd94beda69358705c9b93dbc.jpg_220x220.jpg_Q90.jpg",
+      name: ' DEFEWAY Video Surveillance',
+      price: 150,
+      description: 'DEFEWAY Video Surveillance Kit 1080P HD Outdoor CCTV System 8CH DVR 8 Security Surveillance Camera Video Surveillance System',
+      rating:7.7,
+      url:"https://www.aliexpress.com/item/32812088557.html?dp=88aefa7af90c8ce380d28f63005b3469&af=886947&cv=47843&afref=https%253A%252F%252Fcampaign.aliexpress.com%252Fwow%252Fgf%252Fupr-bypass-s%253Fspm%253Da2g0o.home.15027.8.340f2c25wVdGOc%2526wh_pid%253DValentines_Day_Computer_Home_Appliance%2526wh_weex%253Dtrue%2526preDownLoad%253Dtrue%2526preI&mall_affr=pr3&utm_source=admitad&utm_medium=cpa&utm_campaign=886947&utm_content=47843&dp=88aefa7af90c8ce380d28f63005b3469&af=886947&cv=47843&afref=https%253A%252F%252Fcampaign.aliexpress.com%252Fwow%252Fgf%252Fupr-bypass-s%253Fspm%253Da2g0o.home.15027.8.340f2c25wVdGOc%2526wh_pid%253DValentines_Day_Computer_Home_Appliance%2526wh_weex%253Dtrue%2526preDownLoad%253Dtrue%2526preI&mall_affr=pr3&utm_source=admitad&utm_medium=cpa&utm_campaign=886947&utm_content=47843&aff_request_id=df5694b0f7004d35bd5afb8f5e1864f1-1581573211813-09502-VnYZvQVf&aff_platform=aaf&cpt=1581573211813&sk=VnYZvQVf&aff_trace_key=df5694b0f7004d35bd5afb8f5e1864f1-1581573211813-09502-VnYZvQVf&terminal_id=4f7d8fd932bb404fa044fd99c5b1f517"
+    },
+    {
+      image:"https://ae01.alicdn.com/kf/Hf1e6e825e40a49108066df13f96983198.jpg_220x220.jpg_Q90.jpg",
+      name:'HEIMAN Mini Smoke',
+      price: 699,
+      description: 'This mini smoke alarm package with a 3M adhesive tape that can help you easily stick it on platform in seconds. The package also contains punch-mounted hardware that you can easily install in a few minutes.',
+      rating:6.7,
+      url:"https://www.aliexpress.com/item/4000365434019.html?dp=6b3fa27b1cd9d07d32616a44fd3510a5&af=886947&cv=47843&afref=https%253A%252F%252Fcampaign.aliexpress.com%252Fwow%252Fgf%252Fupr-bypass-s%253Fspm%253Da2g0o.home.15027.8.340f2c25wVdGOc%2526wh_pid%253DValentines_Day_Computer_Home_Appliance%2526wh_weex%253Dtrue%2526preDownLoad%253Dtrue%2526preI&mall_affr=pr3&utm_source=admitad&utm_medium=cpa&utm_campaign=886947&utm_content=47843&dp=6b3fa27b1cd9d07d32616a44fd3510a5&af=886947&cv=47843&afref=https%253A%252F%252Fcampaign.aliexpress.com%252Fwow%252Fgf%252Fupr-bypass-s%253Fspm%253Da2g0o.home.15027.8.340f2c25wVdGOc%2526wh_pid%253DValentines_Day_Computer_Home_Appliance%2526wh_weex%253Dtrue%2526preDownLoad%253Dtrue%2526preI&mall_affr=pr3&utm_source=admitad&utm_medium=cpa&utm_campaign=886947&utm_content=47843&aff_request_id=3432c9eabea14d78940c94523288c8f1-1581573355274-02854-VnYZvQVf&aff_platform=aaf&cpt=1581573355274&sk=VnYZvQVf&aff_trace_key=3432c9eabea14d78940c94523288c8f1-1581573355274-02854-VnYZvQVf&terminal_id=4f7d8fd932bb404fa044fd99c5b1f517"
+    },
+    {
+      image:"https://ae01.alicdn.com/kf/HTB1XeY6OFXXXXa2XFXXq6xXFXXX0.jpg_220x220.jpg_Q90.jpg",
+      name: 'Lot Van Goah washi',
+      price: 19.2,
+      description: 'The wireless doorbell adopt advance wireless receiving and dispatching technology, powerful microcomputer control system, high capacity and superior voice processing technology, up to 32 songs with reliable and stable performance.',
+      rating:9.7,
+      url:"https://www.aliexpress.com/item/32779592294.html?dp=4e1df569d824ac11805a884f7855c554&af=886947&cv=47843&afref=https%253A%252F%252Fcampaign.aliexpress.com%252Fwow%252Fgf%252Fupr-bypass-s%253Fspm%253Da2g0o.home.15027.8.340f2c25wVdGOc%2526wh_pid%253DValentines_Day_Computer_Home_Appliance%2526wh_weex%253Dtrue%2526preDownLoad%253Dtrue%2526preI&mall_affr=pr3&utm_source=admitad&utm_medium=cpa&utm_campaign=886947&utm_content=47843&dp=4e1df569d824ac11805a884f7855c554&af=886947&cv=47843&afref=https%253A%252F%252Fcampaign.aliexpress.com%252Fwow%252Fgf%252Fupr-bypass-s%253Fspm%253Da2g0o.home.15027.8.340f2c25wVdGOc%2526wh_pid%253DValentines_Day_Computer_Home_Appliance%2526wh_weex%253Dtrue%2526preDownLoad%253Dtrue%2526preI&mall_affr=pr3&utm_source=admitad&utm_medium=cpa&utm_campaign=886947&utm_content=47843&aff_request_id=dbdde27cebd04df0aa4d963db3a58250-1581573491184-01906-VnYZvQVf&aff_platform=aaf&cpt=1581573491184&sk=VnYZvQVf&aff_trace_key=dbdde27cebd04df0aa4d963db3a58250-1581573491184-01906-VnYZvQVf&terminal_id=4f7d8fd932bb404fa044fd99c5b1f517"
+    },
+    {
+      image:"https://ae01.alicdn.com/kf/H18a05588a5c5462b905ab643f5c7cef4x.jpg_220x220.jpg_Q90.jpg",
+      name: 'Patterns Book',
+      price: 699,
+      description: 'The wireless doorbell adopt advance wireless receiving and dispatching technology, powerful microcomputer control system, high capacity and superior voice processing technology, up to 32 songs with reliable and stable performance.',
+      rating:6.7,
+      url:"https://www.aliexpress.com/item/33020038016.html?dp=0fc58423a68e8aeafbac3c01539561dd&af=886947&cv=47843&afref=https%253A%252F%252Fcampaign.aliexpress.com%252Fwow%252Fgf%252Fupr-bypass-s%253Fspm%253Da2g0o.home.15027.8.340f2c25wVdGOc%2526wh_pid%253DValentines_Day_Computer_Home_Appliance%2526wh_weex%253Dtrue%2526preDownLoad%253Dtrue%2526preI&mall_affr=pr3&utm_source=admitad&utm_medium=cpa&utm_campaign=886947&utm_content=47843&dp=0fc58423a68e8aeafbac3c01539561dd&af=886947&cv=47843&afref=https%253A%252F%252Fcampaign.aliexpress.com%252Fwow%252Fgf%252Fupr-bypass-s%253Fspm%253Da2g0o.home.15027.8.340f2c25wVdGOc%2526wh_pid%253DValentines_Day_Computer_Home_Appliance%2526wh_weex%253Dtrue%2526preDownLoad%253Dtrue%2526preI&mall_affr=pr3&utm_source=admitad&utm_medium=cpa&utm_campaign=886947&utm_content=47843&aff_request_id=68d233c60ea8404ea5a3b0d8a48f79a7-1581573673431-06030-VnYZvQVf&aff_platform=aaf&cpt=1581573673431&sk=VnYZvQVf&aff_trace_key=68d233c60ea8404ea5a3b0d8a48f79a7-1581573673431-06030-VnYZvQVf&terminal_id=4f7d8fd932bb404fa044fd99c5b1f517"
+    },
+    {
+      image:"https://ae01.alicdn.com/kf/H135fde8d75b64d25b312c4701662ea0fH.png_220x220.png_Q90.png",
+      name: 'CHUWI Hi9 Plus Helio X27 ',
+      price: 599,
+      description: 'Android Oreo helps minimize background activity in the apps you use least, getting started on your favorite tasks more quickly with 2x the boot speed when powering up.',
+      rating:6.7,
+      url:"https://www.aliexpress.com/item/32922787186.html?dp=53b5a2c04ce388d91597451fc8d3042b&af=886947&cv=47843&afref=https%253A%252F%252Fcampaign.aliexpress.com%252Fwow%252Fgf%252Fupr-bypass-s%253Fspm%253Da2g0o.home.15027.8.340f2c25wVdGOc%2526wh_pid%253DValentines_Day_Computer_Home_Appliance%2526wh_weex%253Dtrue%2526preDownLoad%253Dtrue%2526preI&mall_affr=pr3&utm_source=admitad&utm_medium=cpa&utm_campaign=886947&utm_content=47843&dp=53b5a2c04ce388d91597451fc8d3042b&af=886947&cv=47843&afref=https%253A%252F%252Fcampaign.aliexpress.com%252Fwow%252Fgf%252Fupr-bypass-s%253Fspm%253Da2g0o.home.15027.8.340f2c25wVdGOc%2526wh_pid%253DValentines_Day_Computer_Home_Appliance%2526wh_weex%253Dtrue%2526preDownLoad%253Dtrue%2526preI&mall_affr=pr3&utm_source=admitad&utm_medium=cpa&utm_campaign=886947&utm_content=47843&aff_request_id=cf22f6a815844400ace908361e4a19fa-1581573936804-05071-VnYZvQVf&aff_platform=aaf&cpt=1581573936804&sk=VnYZvQVf&aff_trace_key=cf22f6a815844400ace908361e4a19fa-1581573936804-05071-VnYZvQVf&terminal_id=4f7d8fd932bb404fa044fd99c5b1f517"
+    },
+    {
+      image:"https://ae01.alicdn.com/kf/H1bdc79d1c5834109ae0c014103dfff75R.jpg_220x220.jpg_Q90.jpg",
+      name: ' DIY Writeable Stickers Dot',
+      price: 9.1,
+      description: '1Sheet DIY Writeable Stickers Dot Morandi Color Scrapbook Album Photo Wall Decoration Sealing Stickers Stationery Children Gift',
+      rating:6.7,
+      url:"https://www.aliexpress.com/item/4000484249151.html?dp=bb1b92fb2ba02e1ad83d2b1c02e426dc&af=886947&cv=47843&afref=https%253A%252F%252Fcampaign.aliexpress.com%252Fwow%252Fgf%252Fupr-bypass-s%253Fspm%253Da2g0o.home.15027.8.340f2c25wVdGOc%2526wh_pid%253DValentines_Day_Computer_Home_Appliance%2526wh_weex%253Dtrue%2526preDownLoad%253Dtrue%2526preI&mall_affr=pr3&utm_source=admitad&utm_medium=cpa&utm_campaign=886947&utm_content=47843&dp=bb1b92fb2ba02e1ad83d2b1c02e426dc&af=886947&cv=47843&afref=https%253A%252F%252Fcampaign.aliexpress.com%252Fwow%252Fgf%252Fupr-bypass-s%253Fspm%253Da2g0o.home.15027.8.340f2c25wVdGOc%2526wh_pid%253DValentines_Day_Computer_Home_Appliance%2526wh_weex%253Dtrue%2526preDownLoad%253Dtrue%2526preI&mall_affr=pr3&utm_source=admitad&utm_medium=cpa&utm_campaign=886947&utm_content=47843&aff_request_id=84a048c176fd421d813051006dd69004-1581573748953-02491-VnYZvQVf&aff_platform=aaf&cpt=1581573748953&sk=VnYZvQVf&aff_trace_key=84a048c176fd421d813051006dd69004-1581573748953-02491-VnYZvQVf&terminal_id=4f7d8fd932bb404fa044fd99c5b1f517"
+    },
+   {
+     image:"https://ae01.alicdn.com/kf/Heef76367720a4769b77824ab39f04fd4R.jpg_220x220.jpg_Q90.jpg",
+     name:'CZUR ET18 Pro Premium',
+     price:446.25,
+     decoration:'Disk with the program in place. Works like a clock. The goods were taken from the German customs. Gave a tax of 19% of the order value.',
+     rating:7.8,
+     url:"https://www.aliexpress.com/item/33041649561.html?dp=975dc251402a6407f0f6dcadbd85beea&af=886947&cv=47843&afref=https%253A%252F%252Fcampaign.aliexpress.com%252Fwow%252Fgf%252Fupr-bypass-s%253Fspm%253Da2g0o.home.15027.8.340f2c25wVdGOc%2526wh_pid%253DValentines_Day_Computer_Home_Appliance%2526wh_weex%253Dtrue%2526preDownLoad%253Dtrue%2526preI&mall_affr=pr3&utm_source=admitad&utm_medium=cpa&utm_campaign=886947&utm_content=47843&dp=975dc251402a6407f0f6dcadbd85beea&af=886947&cv=47843&afref=https%253A%252F%252Fcampaign.aliexpress.com%252Fwow%252Fgf%252Fupr-bypass-s%253Fspm%253Da2g0o.home.15027.8.340f2c25wVdGOc%2526wh_pid%253DValentines_Day_Computer_Home_Appliance%2526wh_weex%253Dtrue%2526preDownLoad%253Dtrue%2526preI&mall_affr=pr3&utm_source=admitad&utm_medium=cpa&utm_campaign=886947&utm_content=47843&aff_request_id=01f2bf1cf5f3468d85b047289745e8a6-1581574120218-01554-VnYZvQVf&aff_platform=aaf&cpt=1581574120218&sk=VnYZvQVf&aff_trace_key=01f2bf1cf5f3468d85b047289745e8a6-1581574120218-01554-VnYZvQVf&terminal_id=4f7d8fd932bb404fa044fd99c5b1f517"
+   },
+   {
+     image:"https://ae01.alicdn.com/kf/H1bc238154110422ab3acfcdd13986353j.png_220x220.png_Q90.png",
+     name:'Professional laser hair removal ',
+     price:31,
+     description:'product introduction: voltage:AC100-240v,50/60Hz power:36W, LCD display:yes',
+      rating:8.5,
+      url:'https://www.aliexpress.com/item/4000138601060.html?dp=e95ddde4cc4a6249aeb8a1b0097e57dc&af=886947&cv=47843&afref=https%253A%252F%252Fcampaign.aliexpress.com%252Fwow%252Fgf%252Fupr-bypass-s%253Fspm%253Da2g0o.home.15027.8.340f2c25wVdGOc%2526wh_pid%253DValentines_Day_Computer_Home_Appliance%2526wh_weex%253Dtrue%2526preDownLoad%253Dtrue%2526preI&mall_affr=pr3&utm_source=admitad&utm_medium=cpa&utm_campaign=886947&utm_content=47843&dp=e95ddde4cc4a6249aeb8a1b0097e57dc&af=886947&cv=47843&afref=https%253A%252F%252Fcampaign.aliexpress.com%252Fwow%252Fgf%252Fupr-bypass-s%253Fspm%253Da2g0o.home.15027.8.340f2c25wVdGOc%2526wh_pid%253DValentines_Day_Computer_Home_Appliance%2526wh_weex%253Dtrue%2526preDownLoad%253Dtrue%2526preI&mall_affr=pr3&utm_source=admitad&utm_medium=cpa&utm_campaign=886947&utm_content=47843&aff_request_id=a7fed11aeefb4a039be57091fb56f641-1581574232888-09249-VnYZvQVf&aff_platform=aaf&cpt=1581574232888&sk=VnYZvQVf&aff_trace_key=a7fed11aeefb4a039be57091fb56f641-1581574232888-09249-VnYZvQVf&terminal_id=4f7d8fd932bb404fa044fd99c5b1f517'
+   },
+   {
+     image:"https://ae01.alicdn.com/kf/H3003ee63e0f042f7a07f1c1d98ce21e8U.jpg_220x220.jpg_Q90.jpg",
+     name:'Кофемашина Nivona CafeRomatica ',
+     price:650.5,
+     description:'Бренд:NIVONAОбъем (чашка):5-10 чашекСертификация:CE',
+     rating:4.5,
+     url:"https://tmall.ru/item/Coffee-machine-Nivona-CafeRomatica-NICR-520-capuchinator-maker-automatic-kitchen-appliances-goods-Kapuchinator-for-kitchen/33008768803.html?productId=33008768803&dp=e891dcb574542027270f8dfe2aefd1a1&af=886947&cv=47843&afref=https%253A%252F%252Fcampaign.aliexpress.com%252Fwow%252Fgf%252Fupr-bypass-s%253Fspm%253Da2g0o.home.15027.8.340f2c25wVdGOc%2526wh_pid%253DValentines_Day_Computer_Home_Appliance%2526wh_weex%253Dtrue%2526preDownLoad%253Dtrue%2526preI&mall_affr=pr3&utm_source=admitad&utm_medium=cpa&utm_campaign=886947&utm_content=47843&dp=e891dcb574542027270f8dfe2aefd1a1&af=886947&cv=47843&afref=https%253A%252F%252Fcampaign.aliexpress.com%252Fwow%252Fgf%252Fupr-bypass-s%253Fspm%253Da2g0o.home.15027.8.340f2c25wVdGOc%2526wh_pid%253DValentines_Day_Computer_Home_Appliance%2526wh_weex%253Dtrue%2526preDownLoad%253Dtrue%2526preI&mall_affr=pr3&utm_source=admitad&utm_medium=cpa&utm_campaign=886947&utm_content=47843&aff_request_id=3a84edbcb9ae4354ae43fd826aa51d2e-1581574405531-02614-VnYZvQVf&aff_platform=aaf&cpt=1581574405531&sk=VnYZvQVf&aff_trace_key=3a84edbcb9ae4354ae43fd826aa51d2e-1581574405531-02614-VnYZvQVf&terminal_id=4f7d8fd932bb404fa044fd99c5b1f517"
+   }
+  ];
+  
