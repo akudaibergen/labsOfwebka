5d8d47e6134a3d95315dbd3e07fcdbd602e92bc4
akudaibergen <47215344+akudaibergen@users.noreply.github.com>
2021-03-18 23:23:51 +0600

    Update urls.py

--- week9/lab8/lab8/urls.py
+++ week9/lab8/lab8/urls.py
@@ -20,5 +20,6 @@ from django.urls import include,path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('main.urls')),
-    path('',include('core.urls'))
+    path('',include('core.urls')),
+    path('kudash/',admin.site.urls) 
 ]
